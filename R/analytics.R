10e6d45a4bd720edd6e72b7add4cb0150a14ce88
mahsasarafrazi <Ma.sarafrazi@gmail.com>
2022-01-21 10:06:08 -0800

    analytics function script

---
+++ R/analytics.R
@@ -0,0 +1,15 @@
+#' Analyze the clean data frame extracted from twitter website
+#'
+#' @param input_file A character vector with, at most, one element.
+#'
+#'
+#' @return A dataframe vector.
+#' @export
+#'
+#' @examples
+#'x <- "Hello world"
+#' analytics("Hello")
+#'
+analytics <- function(input_file) {
+
+}
