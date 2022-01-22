e158f195b971df4b27ebfe80f0898012008a929e
MAHMOODUR RAHMAN <mahmoodr@student.ubc.ca>
2022-01-22 15:31:32 -0800

    adding plotting function

---
+++ R/plotting.R
@@ -0,0 +1,16 @@
+#' Plot the most frequently occuring hashtags
+#'
+#' @param input_file
+#'        A dataframe
+#'        A column contain string
+#'
+#' @return A plot.
+#' @export
+#'
+#' @examples
+#'plot <- plotting(df, 'col_text')
+#'
+#'
+plotting <- function(df, "col_text") {
+
+}
