7fc61941fab6531a722f4219ef8f5cc07d0e3924
MAHMOODUR RAHMAN <mahmoodr@student.ubc.ca>
2022-01-22 15:11:45 -0800

    Added draft plotting function

---
+++ R/plotting.R
@@ -0,0 +1,16 @@
+#' Plot the most frequent hashtags
+#'
+#' @param input_file
+#' A dataframe.
+#' Name of the text column
+#'
+#' @return A plot
+#' @export
+#'
+#' @examples
+#'(df, "col_text")
+#'
+#'
+analytics <- function(df, "col_text") {
+
+}
