54db83db03258050481f53d725b4aceefc5e4c2b
Shiva Jena <ssjena89@student.ubc.ca>
2022-01-22 16:22:09 -0800

    Feat: Add docstrings to clean_tweets function

---
+++ R/clean_tweets.R
@@ -0,0 +1,25 @@
+#' Plot the most frequently occuring hashtags
+#'
+#' @param file_path
+#'        chr
+#'        File path to csv file containing tweets data
+#'
+#' @param tokenization
+#'        Bool
+#'        Creates new column containing cleaned tweet word tokens when True
+#'        Default is True
+#' @param word_count
+#'        Bool
+#'        Creates new column containing word count of cleaned tweets
+#'        Default is True
+#' @return df_tweets
+#'         Dataframe
+#' .
+#'
+#' @examples
+#'plot <- plotting(df)
+#'
+#'
+clean_tweets <- function(file_path, tokenization) {
+
+}
