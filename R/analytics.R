2d3f324aa3a209cdb81f38c58c1a7c115563fb3d
mahsasarafrazi <Ma.sarafrazi@gmail.com>
2022-01-26 00:31:42 -0800

    adding explanation

--- R/analytics.R
+++ R/analytics.R
@@ -15,14 +15,16 @@ require(sentimentr)
 #' analytics("x")
 #'
 analytics <- function(input_file) {
+    # reading dataframe
     df <- read.csv(input_file)
-
+    
+    # counting number of like, retweet and comments 
     like <-  sum(df$data.public_metrics.like_count)
     comment <- sum(df$data.public_metrics.reply_count)
     retweet <- sum(df$data.public_metrics.retweet_count)
 
     sentiment = c()
-
+    # findning sentiment of each tweet 
     for (i in seq(1, nrow(df), 1)) {
         sent <- sum(sentiment(df$data.text[i])$sentiment)
         if(sent > 0) {
@@ -33,13 +35,15 @@ analytics <- function(input_file) {
             sentiment[i] = "negative"
         }
     }
-
+    # adding sentiment column to the dataframe
     df <- df |> mutate("sentiment"=sentiment)
-
+    
+    # counting the nmber of positive, nuetral and negative sentiments
     pos_sent <- df |>  filter(sentiment == 'positive') |> nrow()
     neut_sent <-  df |>  filter(sentiment == 'neutral') |> nrow()
     neg_sent <- df |>  filter(sentiment == 'negative') |> nrow()
-
+    
+    # tibble of result
     results = tibble("metrics"=c("Total Number of Likes",
                                  "Total Number of Comments",
                                  "Total Number of Retweets",
