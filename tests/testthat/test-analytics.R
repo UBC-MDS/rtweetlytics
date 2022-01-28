9036d40e604437495627de9bedf167c9713370dc
mahsasarafrazi <Ma.sarafrazi@gmail.com>
2022-01-28 11:03:31 -0800

    adding more tests

--- tests/testthat/test-analytics.R
+++ tests/testthat/test-analytics.R
@@ -1,11 +1,16 @@
+
+
+#testing number of likes
 test_that("analytics() total likes", {
   expect_equal(analytics("output/tweets_response.csv")$Analytics[1], 13)
 })
 
+#testing number of comments
 test_that("analytics() total comments", {
   expect_equal(analytics("output/tweets_response.csv")$Analytics[2], 4)
 })
 
-test_that("analytics() total comments", {
+#testing number of retweets
+test_that("analytics() total retweets", {
   expect_equal(analytics("output/tweets_response.csv")$Analytics[3], 2657)
 })
